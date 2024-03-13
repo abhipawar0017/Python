31e17d885403c58c35637c29a5ba7425913e74c0
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-13 21:06:09 +0530

    Delete 02_Data Type and Variable/Practice_Set_05.py

--- 02_Data Type and Variable/Practice_Set_05.py
+++
@@ -1,8 +0,0 @@
-a = input("Enter a First Number: ")
-b = input("Enter a Second Number: ")
-a = int(a)
-b = int(b)
-sum = a + b
-average = sum / 2
-print("The Sum of Two Numbers is: ", sum)
-print("The Average of Two Number is: ", average)
