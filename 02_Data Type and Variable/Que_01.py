de73e50329ca6818beaccbf22b18bfb118bd935f
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-13 21:06:48 +0530

    Create Que_01.py

---
+++ 02_Data Type and Variable/Que_01.py
@@ -0,0 +1,7 @@
+#Question-1
+# Program to find the Square root of a number
+
+import math
+a = int(input("Enter a number : "))
+print("Square root of number is :",int(math.sqrt(a)))
+print("Exact square root of number is :",math.sqrt(a))
