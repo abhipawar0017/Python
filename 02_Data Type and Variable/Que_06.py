ea4ee79cafba29fc0ff8494d76b30f721acbce91
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-13 21:08:58 +0530

    Create Que_06.py

---
+++ 02_Data Type and Variable/Que_06.py
@@ -0,0 +1,9 @@
+#Question-6
+# Write Logical operators conditions with taking two variables
+
+a=int(input("Enter a :"))
+b=int(input("Enter b :"))
+c=int(input("Enter c :"))
+
+print(a>b and b>c)
+print(a>b or b>c)
