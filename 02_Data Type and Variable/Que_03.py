5f0ed44c802529319f1a009c2e3fae56fc1118c6
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-13 21:07:47 +0530

    Create Que_03.py

---
+++ 02_Data Type and Variable/Que_03.py
@@ -0,0 +1,8 @@
+#Question-3
+# Program to calculate the area  and Perimeter of the Square
+
+l=int(input("Enter the length of rectangle :"))
+A = l**2
+P = 4*l
+print("Area os square is :",A)
+print("Perimeter of square is :",P)
