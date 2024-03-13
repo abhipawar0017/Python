23b3ea260040f096db4d12a7ac444fb13b34940b
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-13 21:08:10 +0530

    Create Que_04.py

---
+++ 02_Data Type and Variable/Que_04.py
@@ -0,0 +1,10 @@
+#Question-4
+# program to Calculate Surface Volumne and Area of Cylinder
+
+r=int(input("Enter the radius:"))
+h=int(input("Enter the height:"))
+V= 3.14*(r**2)*h
+A= 2*3.14*r*(r+h)
+
+print("Volume of cylinder is :",V)
+print("Area os cylinder is :",A)
