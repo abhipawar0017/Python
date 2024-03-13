764ff087d2340aedffb3170b2b78c48db871973e
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-13 20:35:38 +0530

    Delete 01_Introduction directory

--- 01_Introduction/01_Hello_world.py
+++
@@ -1,6 +0,0 @@
-# Hello My Name is Abhi
-'''
-Abhi
-'''
-print("Hello World")
-print('''Abhi''')
--- 01_Introduction/01_Question.py
+++
@@ -1,7 +0,0 @@
-# Pawar Abhishek Rohidas
-# Roll No. 242
-# final Year
-print('''Abhi..''')
-print("Abhi..")
-print('Abhi..')
-
