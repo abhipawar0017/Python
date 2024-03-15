a5c4e1b595912d8f947b2d1d43a26468a0940617
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-16 00:07:38 +0530

    Update Que_05.py

--- 04_Dictionary_and_set/Que_05.py
+++ 04_Dictionary_and_set/Que_05.py
@@ -1,2 +1,14 @@
-s = { }
-print(type(s))
+language1 = input("Enter Abhi Language: ")
+language2 = input("Enter Sun Language: ")
+language3 = input("Enter Tush Language: ")
+language4 = input("Enter Sam Language: ")
+language5 = input("Enter Ram Language: ")
+
+myDict = {"Abhi": language1,
+          "Sun": language2,
+          "Tush": language3,
+          "Sam": language4,
+          "Ram": language5 }
+
+print(myDict.values())
+print(myDict.items())
