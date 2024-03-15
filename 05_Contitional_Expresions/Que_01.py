221827254a284816dfa91c10e7e015ed34af1ffe
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-16 00:16:58 +0530

    Create Que_01.py

---
+++ 05_Contitional_Expresions/Que_01.py
@@ -0,0 +1,17 @@
+## Question
+## +,-,*,/ operations
+
+n = int(input("Enter the 1st number : "))
+m = int(input("Enter the 2nd number : "))
+o = input("Enter sign from +,-,x,/ : ")
+
+if(o == "+"):
+  print(f"Addition : {n+m}",)
+elif(o == "-"):
+  print(f"Addition : {n-m}",)
+elif(o == "x"):
+  print(f"Addition : {n*m}",)
+elif(o == "/"):
+  print(f"Addition : {n/m}",)
+else:
+  print("Entered wrong symbol.")
