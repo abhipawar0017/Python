a4474346c785cc714535478e549dd17466da17cc
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-16 00:29:07 +0530

    Create Que_03.py

---
+++ 06_while_looping/Que_03.py
@@ -0,0 +1,15 @@
+# Program to print the table of 7 I want O/P as in given format
+# 7 x 1 = 7
+# 7 x 2 = 14
+# 7 x 3 = 21
+# .
+# .
+# .
+# .
+# 7 x 10 = 70
+
+n =7
+i=1
+while(i<=10):
+  print(" 7 x",i,"=", (7*i))
+  i=i+1
