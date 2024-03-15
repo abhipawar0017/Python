7bfea5b480b0682d038d092f34a3b5b2b2a87de3
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-15 23:44:24 +0530

    Create 1_list.py

---
+++ 03_Lists and Tuples/1_list.py
@@ -0,0 +1,12 @@
+#example
+list = [70, 22, 90, 466, 80]
+
+# Printing The Element in the List
+print(list)
+
+# Print Element at Specific Position
+print(list[0])
+
+# Set The Element at The Specific Position
+list[0] = 100
+print(list)
