74bca467a1383c067ad217204b7746203e0ccb5b
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-15 23:42:45 +0530

    Delete 04_Lists and Tuples directory

--- 04_Lists and Tuples/02_List.py
+++
@@ -1,9 +0,0 @@
-list = ["Shubham", 244, False, 90.90]
-print(list[0])
-print(list[1])
-print(list[2])
-print(list[3])
-
-print(list[0:])
-print(list[-1])
-print(list[-4:-1])
--- 04_Lists and Tuples/Problem_No05.py
+++
@@ -1,2 +0,0 @@
-a = (7, 0, 8, 0, 0, 9)
-print("The Number of Zeros Present is: ", a.count(0))
