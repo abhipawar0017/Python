4af93a00de516dcd371febaa8c1e5e6a90483445
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-16 00:12:53 +0530

    Create 03_condition.py

---
+++ 05_Contitional_Expresions/03_condition.py
@@ -0,0 +1,13 @@
+# elif
+
+i = int(input("Enter a number"))
+if(i==10):
+  print("i is 10")
+elif(i==15):
+  print("i is 15")
+elif(i==25):
+  print("i is 25")
+elif(i==20):
+  print("i is 20")
+else:
+  print("Number is other than 10,15,25,20")
