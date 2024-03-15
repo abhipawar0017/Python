a7eb75c704aca5ad5a2f91ef1b01bf7f4bb88944
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-16 00:22:39 +0530

    Create 02_while.py

---
+++ 06_while_looping/02_while.py
@@ -0,0 +1,10 @@
+#Print a series of number from one to 70 if you ur nos is 20 then print nos is 20 and if your nos is 50 print nos is 50
+
+n = 1
+while(n<=70):
+  print(n)
+  if(n==20):
+    print("Number is 20")
+  elif(n==50):
+    print("Number is 50")
+  n=n+1
