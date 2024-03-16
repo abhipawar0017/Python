dc0234911043b81a8cd3b1c724953a464f52326d
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-16 23:31:39 +0530

    Create 02_for.py

---
+++ 07_for_looping/02_for.py
@@ -0,0 +1,8 @@
+## Printing odd numbers
+n = int(input("Enter the range:"))
+print(list(range(1,n,2)))
+
+
+#Write a program to print the series from 1 to 10 without using while loop:--
+for i in range(1,11):
+  print(i)
