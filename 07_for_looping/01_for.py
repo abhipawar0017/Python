0add8d86af78b6f25116d0ea15a2cbf569852705
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-16 23:30:37 +0530

    Create 01_for.py

---
+++ 07_for_looping/01_for.py
@@ -0,0 +1,11 @@
+#   Syntax :-
+#for varaible_name in condition:
+#  statements
+
+
+l = [10,20,30,40,50,60,70,80,90,100]
+print("Before Looping" , l)
+print("After Looping:--")
+for i in l:
+  print(i)
+
