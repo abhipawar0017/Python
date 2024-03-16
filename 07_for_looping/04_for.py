9e0568cfb04f40b32c5537bee2128587a6b96bf6
Abhishek Pawar <abhipawar1910@gmail.com>
2024-03-16 23:33:02 +0530

    Create 04_for.py

---
+++ 07_for_looping/04_for.py
@@ -0,0 +1,22 @@
+#range function :
+# syntax :
+# range(starting_nos , last_number , step)
+
+# starting_nos : from where you wnt to start it  , by default it will be zero
+# Last_nos : that is end of ur sequence(End-1)
+
+print(list(range(1 ,101)))
+
+print(list(range(1 ,101, 2))) # increment by 1
+
+print(list(range(1 ,101, 3))) # increment by 2
+
+print(list(range(101 ,1, -2))) # increment by 2
+
+print(list(range(-4 ,4))) #Negative Integer
+
+print(list(range(0,1)))
+
+print(list(range(1,1)))
+
+print(list(range(0)))
