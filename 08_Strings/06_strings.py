9da064575e2b1d82d03b9b63e5f67f0d3172fd0a
Abhishek Pawar <abhipawar1910@gmail.com>
2024-04-02 17:29:49 +0530

    Create 06_strings.py

---
+++ 08_Strings/06_strings.py
@@ -0,0 +1,10 @@
+a = "Abhisshek Pawar"
+
+print("1.",a.capitalize())
+print("2.",a.count("s"))
+print("3.",a.count("a",10,20))
+
+#enumerate():-- Assigns sequence
+s2 = ["Orange" , "Mango" , "Banana" , "PineApple"]
+s3 = enumerate(s2)
+print(list(s3))
