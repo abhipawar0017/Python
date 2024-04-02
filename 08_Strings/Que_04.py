b11408bfe9b259c54d598a274153fe68149d047a
Abhishek Pawar <abhipawar1910@gmail.com>
2024-04-02 17:31:45 +0530

    Create Que_04.py

---
+++ 08_Strings/Que_04.py
@@ -0,0 +1,12 @@
+# Write a program to replace all the occurence of 'a' with $ in a string
+
+val = "abhishek pawar"
+new = val.replace('a','$')
+print(new)
+
+# Program to get a single string from two given strings  , seprated by a and swap the first two characters of each string
+# Sample String :- 'abc','xyz'
+# Expected Output :- 'xyzabc'
+
+l = ('abc', 'xyz') 
+print(l[1]+l[0])
