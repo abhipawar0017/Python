bb107eb00bb3ecde804b529f257efbce16c6fd12
Abhishek Pawar <abhipawar1910@gmail.com>
2024-04-02 17:28:41 +0530

    Create 04_strings.py

---
+++ 08_Strings/04_strings.py
@@ -0,0 +1,16 @@
+#String Speical Operators :---
+# + :- Concatenations of the string
+# * :- Repetition of the strings
+# [] :- Slice
+# [:] :- Range of slice
+# in  and not in :- membership
+# % :- String  Formatting
+
+## +
+a= "Abhi"
+b= " Pawar"
+print("Concate : ",a+b)
+
+## *
+a = "Abhi "
+print(a*7)
