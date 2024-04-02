3378b14e888db2812a88ce238e5e8af5a1a624fd
Abhishek Pawar <abhipawar1910@gmail.com>
2024-04-02 17:26:55 +0530

    Create 03_strings.py

---
+++ 08_Strings/03_strings.py
@@ -0,0 +1,11 @@
+## Escape Charcaters ##
+
+# \n :- newline
+# \b:- backspace
+# \xnos :- hex representations
+
+
+print("1. This is printed\n in two lines")
+print("2. This is printed\b in two lines")
+print("3. This is \x48\x45\x58 representations")
+print("4. This is \x77")
