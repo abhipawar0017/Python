6e230656cf5980be340557f1c4bcb298d5beb37d
Abhishek Pawar <abhipawar1910@gmail.com>
2024-04-02 17:25:36 +0530

    Update 1_strings.py

--- 08_Strings/1_strings.py
+++ 08_Strings/1_strings.py
@@ -1,12 +1,11 @@
-#Program to check the nos is palindrome or not
-n1 = int(input("Enter the num :"))
-rev = 0
-n2 = n1
-while n2 >0:
-  rem=n2 % 10
-  rev= rev*10 + rem
-  n2=int(n2/10)
-if (n1==rev):
-  print("Number is palindrome")
-else:
-  print("Number is not palindrome")
+# Strings :-
+#It is the Sequence of characters or combinations of it
+
+single_quoted = 'Abhi'
+print("The Single Quoted String is :--" , single_quoted)
+
+double_quoted = "Pawar"
+print("The Double Quoted String is :--" , double_quoted) #most usable one
+
+my_string = '''  ram RAM'''
+print(my_string)
