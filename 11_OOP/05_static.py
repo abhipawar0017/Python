afa70ddafda23957b2c7e61200bb0adcce3f7cfd
Abhishek Pawar <abhipawar1910@gmail.com>
2024-07-02 10:11:31 +0530

    Create 05_static.py

---
+++ 11_OOP/05_static.py
@@ -0,0 +1,17 @@
+class Greet:
+    @staticmethod
+    def Gm():
+        print("Good Morning Abhi")
+    
+    @staticmethod
+    def Ge():
+        print("Good Evening Abhi")
+
+    @staticmethod
+    def Gn():
+        print("Good Night Abhi")
+
+abhi = Greet()
+abhi.Gm()
+abhi.Ge()
+abhi.Gn()
